976da5594966ece0f3751a9807771eac5a04404f
Roc Lai <laipeng668@qq.com>
2026-02-03 21:22:51 +0800

    Fix image generation for factory.bin

--- target/linux/qualcommax/image/ipq807x.mk
+++ target/linux/qualcommax/image/ipq807x.mk
@@ -634,6 +634,7 @@ define Device/inseego_fg2000
     SOC := ipq8072
 	KERNEL_SIZE := 6144k
 	DEVICE_PACKAGES := kmod-hwmon-gpiofan kmod-ath11k-ahb kmod-phy-aquantia kmod-fs-f2fs f2fs-tools kmod-usb3 kmod-usb-dwc3-qcom
-	IMAGES += factory.bin := append-kernel | pad-to $$$$(KERNEL_SIZE) | append-rootfs
+	IMAGES += factory.bin
+	IMAGE/factory.bin := append-kernel | pad-to $$$$(KERNEL_SIZE) | append-rootfs
 endef
 TARGET_DEVICES += inseego_fg2000
